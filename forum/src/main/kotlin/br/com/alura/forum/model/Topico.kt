f0de035b6796fb8574fe43a6eff280c7b81b9c20
Weslly Viana <devweslly@gmail.com>
2022-07-27 17:39:53 -0300

    Listagem de tópicos

--- forum/src/main/kotlin/br/com/alura/forum/model/Topico.kt
+++ forum/src/main/kotlin/br/com/alura/forum/model/Topico.kt
@@ -10,5 +10,5 @@ data class Topico(
     val curso: Curso,
     val autor: Usuario,
     val status: StatusTopico = StatusTopico.NAO_RESPONDIDO,
-    val respostas: List<Resposta> = arrayList()
+    val respostas: List<Resposta> = ArrayList()
 )
