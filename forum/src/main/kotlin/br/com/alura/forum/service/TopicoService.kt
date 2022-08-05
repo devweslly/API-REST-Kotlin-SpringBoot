58149e63cc5250d12faf17bba42b1f1d142cd5d9
Weslly Viana <devweslly@gmail.com>
2022-08-04 23:30:39 -0300

    Busca de tópico pelo id

--- forum/src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
+++ forum/src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
@@ -7,8 +7,10 @@ import org.springframework.stereotype.Service
 import java.util.*
 
 @Service
-class TopicoService {
-    fun listar(): List<Topico> {
+class TopicoService(private var topicos: List<Topico>) {
+
+    // Bloco de inicialização
+    init {
         val topico = Topico(
             id = 1,
             titulo = "Duvidas Kotlin",
@@ -25,6 +27,47 @@ class TopicoService {
             )
         )
 
-        return Arrays.asList(topico, topico, topico)
+        val topico2 = Topico(
+            id = 2,
+            titulo = "Duvidas Kotlin 2",
+            mensagem = "Variáveis no Kotlin 2",
+            curso = Curso(
+                id = 1,
+                nome = "Kotlin",
+                categoria = "Programação"
+            ),
+            autor = Usuario(
+                id = 1,
+                nome = "Ana da Silva",
+                email = "ana@email.com"
+            )
+        )
+
+        val topico3 = Topico(
+            id = 3,
+            titulo = "Duvidas Kotlin 3",
+            mensagem = "Variáveis no Kotlin 3",
+            curso = Curso(
+                id = 1,
+                nome = "Kotlin",
+                categoria = "Programação"
+            ),
+            autor = Usuario(
+                id = 1,
+                nome = "Ana da Silva",
+                email = "ana@email.com"
+            )
+        )
+
+        topicos =  Arrays.asList(topico, topico2, topico3)
+    }
+    fun listar(): List<Topico> {
+        return topicos
+    }
+
+    fun buscarPorId(id: Long): Topico {
+        return topicos.stream().filter({
+            t -> t.id == id
+        }).findFirst().get() 
     }
 }
