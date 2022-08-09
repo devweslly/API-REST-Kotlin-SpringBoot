677611d1ef26e27acf58efa28ea2707ff120f8a5
Weslly Viana <devweslly@gmail.com>
2022-08-09 11:53:16 -0300

    Tratamento de erro 400 na API

--- forum/src/main/kotlin/br/com/alura/forum/dto/NovoTopicoForm.kt
+++ forum/src/main/kotlin/br/com/alura/forum/dto/NovoTopicoForm.kt
@@ -5,8 +5,10 @@ import javax.validation.constraints.NotNull
 import javax.validation.constraints.Size
 
 data class NovoTopicoForm(
-    @field:NotEmpty @field:Size(min = 5, max = 100) val titulo: String,
-    @field:NotEmpty val mensagem: String,
+    @field:NotEmpty(message = "Título não pode ficar vazia")
+    @field:Size(min = 5, max = 100, message = "Título deve ter entre 5 e 100 caracteres")
+    val titulo: String,
+    @field:NotEmpty(message = "Mensagem não pode ficar vazia") val mensagem: String,
     @field:NotNull val idCurso: Long,
     @field:NotNull val idAutor: Long,
 )
