df45f588ad7079af378b64028691ab136ac8a205
Weslly Viana <devweslly@gmail.com>
2022-08-09 11:30:04 -0300

    Tratamento de erro 500 na API

--- forum/src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
+++ forum/src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
@@ -28,7 +28,7 @@ class TopicoService(
     fun buscarPorId(id: Long): TopicoView {
         val topico = topicos.stream().filter { t ->
             t.id == id
-        }.findFirst().orElseThrow {NotFoundException(notFoundMessage)}
+        }.findFirst().orElseThrow { NotFoundException(notFoundMessage) }
         return topicoViewMapper.map(topico)
     }
 
@@ -42,7 +42,7 @@ class TopicoService(
     fun atualizar(form: AtualizacaoTopicoForm): TopicoView {
         val topico = topicos.stream().filter { t ->
             t.id == form.id
-        }.findFirst().orElseThrow {NotFoundException(notFoundMessage)}
+        }.findFirst().orElseThrow { NotFoundException(notFoundMessage) }
         val topicoAtualizado = Topico(
             id = form.id,
             titulo = form.titulo,
@@ -60,7 +60,7 @@ class TopicoService(
     fun deletar(id: Long) {
         val topico = topicos.stream().filter { t ->
             t.id == id
-        }.findFirst().orElseThrow {NotFoundException(notFoundMessage)}
+        }.findFirst().orElseThrow { NotFoundException(notFoundMessage) }
         topicos = topicos.minus(topico)
     }
 }
